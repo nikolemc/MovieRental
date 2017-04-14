e01506442edfdfaf8ba8dd89c794eebfec8cf4cf
nikolemc <nikolemcstanley@gmail.com>
2017-04-14 14:26:35 -0400

    Create Genre

---
+++ MovieRental/Controllers/GenresController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MovieRental.Models;
+using MovieRental.Services;
+
+namespace MovieRental.Controllers
+{
+    public class GenresController : Controller
+    {
+        // GET: Genre
+        public ActionResult Index()
+        {
+            // get all genres
+            var genres = new GenreServices().GetAllGenres();
+            // pass them to the view
+            return View("Index", genres);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            var Genre = collection["Genre"];
+
+            var newGenre = new Genres 
+            {
+                Genre = Genre,
+            };
+
+            var updateCustomer = new Genres 
+            {
+                Genre = Genre,
+
+            };
+            new GenreServices().AddGenre(newGenre);
+
+            // TODO: Put into our database
+            return RedirectToAction("Index");
+        }
+
+    }
+
+
+}
---
+++ MovieRental/Services/GenreServices.cs
@@ -0,0 +1,55 @@
+﻿using MovieRental.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using MovieRental.ViewModel;
+
+namespace MovieRental.Services
+{
+    public class GenreServices
+    {
+        const string connectionString = @"Server=localhost\SQLEXPRESS;Database=MovieRentalData;Trusted_Connection=True;";
+
+        public List<Genres> GetAllGenres()
+        {
+            var rv = new List<Genres>();
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var query = "SELECT * FROM [GenreTable]";
+                var cmd = new SqlCommand(query, connection);
+                connection.Open();
+                var reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    rv.Add(new Genres(reader));
+                }
+                connection.Close();
+            }
+            return rv;
+
+        }
+
+        public void AddGenre(Genres genres)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+
+                var newgenre = @"INSERT INTO GenreTable (Genre)" +
+                "Values (@Genre)";
+
+                var sqlCommand = new SqlCommand(newgenre, connection);
+
+                sqlCommand.Parameters.AddWithValue("@Genre", genres.Genre);
+                connection.Open();
+                sqlCommand.ExecuteNonQuery();
+                connection.Close();
+
+            }
+
+        }
+
+
+    }
+}
